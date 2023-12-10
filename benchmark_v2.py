83c34fd127680077bb06a0758e711dc4a5fff15c
Albanene <rasec0cesar@gmail.com>
2023-12-09 20:50:19 -0600

    get the fminist and cifar10 datasets

--- benchmark_v2.py
+++ benchmark_v2.py
@@ -13,7 +13,14 @@ import cpuinfo                      #pip install py-cpuinfo
 # for ml models
 import tensorflow as tf
 from tensorflow import keras
-import sklearn as skl
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler                                                        # to standarize
+from sklearn.decomposition import PCA                                                                   # to standarize
+from sklearn.ensemble import RandomForestClassifier                                                     # model
+from sklearn.linear_model import LogisticRegression                                                     # model
+from sklearn.svm import SVC                                                                             # model
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis     # model
 # misc libraries
 import multiprocessing
 import os
@@ -110,27 +117,57 @@ def arguments():
     scan_parser = subparser.add_parser("scan", help="scan the current hardware") # scan no necesita tener niguna opción
     show_subparser = subparser.add_parser("show", help="display the list of saved benchmarks") # maybe argumentos posicionales del primer y último benchmark
     run_subparser = subparser.add_parser("run", help="options for running the current benchmark") # run si tiene que tener argumentos 
-    run_subparser.add_argument("-H","--hardware",choices=[1,2], default=1, type=int, nargs=1, help="select the hardware in which the code is ran")
-    run_subparser.add_argument("-p","--processor", choices=[1,2,3,4,5,6,7,8], default=1, type=int, nargs=1, help="select the quantity of processors that can be used in the training")
-    run_subparser.add_argument("-m","--model",choices=[1,2,3], default=1, type=int, nargs=1, help="select the ML model")
-    run_subparser.add_argument("-d","--dataset", choices=[1,2,3,4,5], default=5, type=int, nargs=1, help="select the dataset to train the ML model")
+    run_subparser.add_argument("-H","--hardware",choices=[1,2], default=1, type=int, help="select the hardware in which the code is ran")
+    run_subparser.add_argument("-p","--processor", choices=[1,2,3,4,5,6,7,8], default=1, type=int, help="select the quantity of processors that can be used in the training")
+    run_subparser.add_argument("-m","--model",choices=[1,2,3], default=1, type=int, help="select the ML model")
+    run_subparser.add_argument("-d","--dataset", choices=[1,2,3,4,5], default=5, type=int, help="select the dataset to train the ML model")
     run_subparser.add_argument("-i","--iteration", action="store", help="Number of repeated trainings for each algorithm (int, default = 1000)",
                         type=int,default=1000,dest='iteration',metavar='Iterations') # change the number to 1000, for practicity i used 100
     save_subparser = subparser.add_parser("save", help="save current benchmark") # quizá un argumento posicional de nombre
     ###### NOTA: revisar el nargs, para que solo pueda admitir un solo argumento
     return parser.parse_args()
 
-def dataset_processing(data):
-    if data==1:
-        print(data)
-    elif data==2:
-        print(data)
-    elif data==3:
-        print(data)
-    elif data==4:
-        print(data)
-    elif data==5:
-        print(data)
+def dataset_processing(num):
+    scaler = StandardScaler()
+    if num==1:
+        f_mnist = keras.datasets.fashion_mnist
+        # Separate the data from fmnist into train and test data
+        (x_train,y_train),(x_test,y_test) = f_mnist.load_data()
+        # Copy the data so we can reshape a 2D matrix
+        xc_train = x_train.copy()
+        yc_train = y_train.copy()
+        xc_test = x_test.copy()
+        yc_test = y_test.copy()
+        nshape = xc_train.shape[1]*xc_train.shape[2]
+        xc_train = np.reshape(xc_train,[xc_train.shape[0],nshape])
+        xc_test	= np.reshape(xc_test,[xc_test.shape[0],nshape])
+        xc_train_std = scaler.fit_transform(xc_train)
+        xc_test_std = scaler.transform(xc_test)
+        print("The fashion mnist dataset has been chosen")
+        return xc_train_std, yc_train, xc_test_std, yc_test
+    elif num==2:
+        digits = keras.datasets.cifar10
+        (x_train,y_train),(x_test,y_test) = digits.load_data()
+        # one hot encoding labels
+        y_train_encoded = keras.utils.to_categorical(y_train, num_classes = 10, dtype = 'float32')
+        y_test_encoded = keras.utils.to_categorical(y_test, num_classes = 10, dtype = 'float32')
+        xc_train = x_train.copy()
+        yc_train = y_train.copy()
+        xc_test = x_test.copy()
+        yc_test = y_test.copy()
+        xc_train_std = scaler.fit_transform(xc_train)
+        xc_test_std = scaler.transform(xc_test)
+        print("The digits dataset has been chosen")
+        return xc_train_std, yc_train, xc_test_std, yc_test
+    elif num==3:
+        return (num)
+    elif num==4:
+        return (num)
+    elif num==5:
+        return (num)
+    
+def get_model(value):
+    return value
 
 ## Necesitamos empezar el programa al menos con un dataset, un modelo de ML y un numero determinado de iteraciones
 def main_func():
@@ -141,18 +178,18 @@ def main_func():
         system = scan_hardware()
     if args.subcommand == "show":
         # Para que muestre el benchmark que llevamos, se deberá guardar en un html o un archivo por separado\
-        print("benchmark salvados")
+        print("Saved benchmarks")
     if args.subcommand == "run":
         # Para que corra el código con todos los posibles escenarios, deberá correr al menos un modelo con un dataset
         print(args)
         if args.hardware == 1:
-            print("corriendo el programa en el cpu")
-            print(args.iteration)
+            print("Running on CPU")
+            # print(args.iteration)
             dataset_processing(args.dataset)
         if args.hardware == 2:
-            print("corriendo el programa en la gpu")
+            print("Running on GPU")
     if args.subcommand == "save":
-        print("El benchmark se ha guardado correctamente")
+        print("The benchmark has been saved succesfully")
 
 ##### NOTA 2: el resultado que queremos observar es unicamemnte en el tiempo de ejecución
 
